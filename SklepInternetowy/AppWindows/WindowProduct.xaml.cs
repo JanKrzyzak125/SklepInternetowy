f011283265ac30c9d4004867cec2e6d8e0ae362f
Mucha125 <jankrzy9999@gmail.com>
2021-11-30 14:33:47 +0100

    Ujednolicono styl nazw zmiennych

--- SklepInternetowy/AppWindows/WindowProduct.xaml.cs
+++ SklepInternetowy/AppWindows/WindowProduct.xaml.cs
@@ -2,43 +2,43 @@
 
 namespace SklepInternetowy
 {
-    /// <summary>
-    /// Logika interakcji dla klasy WindowProduct.xaml
-    /// </summary>
-    public partial class WindowProduct : Window
-    {
-        private WindowPay _windowPay;
-        public WindowProduct()
-        {
-            InitializeComponent();
-        }
+	/// <summary>
+	/// Logika interakcji dla klasy WindowProduct.xaml
+	/// </summary>
+	public partial class WindowProduct : Window
+	{
+		private WindowPay windowPay;
+		public WindowProduct()
+		{
+			InitializeComponent();
+		}
 
-        private void Click_Pay(object sender, RoutedEventArgs e) 
-        {
-            _windowPay =new WindowPay();
-            _windowPay.Show();
-        }
+		private void Click_Pay(object sender, RoutedEventArgs e)
+		{
+			windowPay = new WindowPay();
+			windowPay.Show();
+		}
 
-        void ShowBuyerProduct() 
-        {
-            EditProduct.Visibility = Visibility.Hidden;
-            _windowPay.Visibility = Visibility.Visible;
-        }
+		void ShowBuyerProduct()
+		{
+			EditProduct.Visibility = Visibility.Hidden;
+			windowPay.Visibility = Visibility.Visible;
+		}
 
-        void ShowSellerProduct() 
-        {
-            EditProduct.Visibility = Visibility.Visible;
-            _windowPay.Visibility = Visibility.Hidden;
-        }
+		void ShowSellerProduct()
+		{
+			EditProduct.Visibility = Visibility.Visible;
+			windowPay.Visibility = Visibility.Hidden;
+		}
 
-        private void EditProduct_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
+		private void EditProduct_Click(object sender, RoutedEventArgs e)
+		{
 
-        private void ChangePhoto_Click(object sender, RoutedEventArgs e)
-        {
+		}
 
-        }
-    }
+		private void ChangePhoto_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
 }
